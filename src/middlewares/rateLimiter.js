3c2b57a730c41df5d5b71e81fbff787832493d22
Mohammad Houda <124600512+mohammadhouda@users.noreply.github.com>
2026-01-17 22:52:56 +0200

    security updates

--- src/middlewares/rateLimiter.js
+++ src/middlewares/rateLimiter.js
@@ -1,8 +1,8 @@
 import rateLimit from "express-rate-limit";
 
 export const rateLimiter = rateLimit({
-    windowMs: 10 * 60 * 1, // 10 minutes
-    max: 20, // limit each IP to 5 requests per windowMs
+    windowMs: 10 * 60 * 1000, // 10 minutes
+    max: 10, // limit each IP to 10 requests per windowMs
     message: "Too many requests from this IP, please try again later.",
     standardHeaders: true,
     skipSuccessfulRequests: true
